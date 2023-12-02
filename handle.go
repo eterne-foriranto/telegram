21d781514c8472e1b3809137406b8bd165edab12
Yevgeni Starostin <erafaschismus@gmail.com>
2023-12-02 17:21:54 +0300

    feat: implement function handling input message

---
+++ handle.go
@@ -0,0 +1,36 @@
+package main
+
+import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+
+type Response struct {
+	Text   string
+	Keys   []tgbotapi.KeyboardButton
+	ChatID int64
+}
+
+type State struct {
+	OwnerChatID int64
+}
+
+func (s State) handle(msg *tgbotapi.Message) Response {
+	if msg.Chat.ID == s.OwnerChatID {
+		return Response{
+			Text:   "Привет!",
+			Keys:   nil,
+			ChatID: msg.Chat.ID,
+		}
+	}
+	return Response{
+		Text:   "",
+		Keys:   nil,
+		ChatID: msg.Chat.ID,
+	}
+}
+
+func makeMessage(resp Response) tgbotapi.MessageConfig {
+	msg := tgbotapi.NewMessage(resp.ChatID, resp.Text)
+	if resp.Keys != nil {
+		msg.ReplyMarkup = tgbotapi.NewReplyKeyboard(resp.Keys)
+	}
+	return msg
+}
