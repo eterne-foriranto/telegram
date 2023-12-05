f59cdb2aed676170adbf119016fce2768e3cbf96
Yevgeni Starostin <erafaschismus@gmail.com>
2023-12-06 01:02:02 +0300

    fix: buttons

--- handle.go
+++ handle.go
@@ -3,36 +3,35 @@ package main
 import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
 type Response struct {
-	Text   string
-	Keys   []string
-	ChatID int64
+	Text    string
+	Buttons []string
+	ChatID  int64
 }
 
 func (a App) handle(msg *tgbotapi.Message) Response {
 	if int(msg.Chat.ID) == a.Owner.ChatID {
-		//keys := make([]string, 0)
-		//append(keys, "invite")
 		return Response{
-			Text:   "Привет!",
-			Keys:   []string{"invite"},
-			ChatID: msg.Chat.ID,
+			Text:    "Привет!",
+			Buttons: []string{"invite"},
+			ChatID:  msg.Chat.ID,
 		}
 	}
 	return Response{
-		Text:   "",
-		Keys:   nil,
-		ChatID: msg.Chat.ID,
+		Text:    "",
+		Buttons: nil,
+		ChatID:  msg.Chat.ID,
 	}
 }
 
 func makeMessage(resp Response) tgbotapi.MessageConfig {
 	msg := tgbotapi.NewMessage(resp.ChatID, resp.Text)
 	buttons := make([]tgbotapi.KeyboardButton, 0)
-	for _, v := range resp.Keys {
-
+	for _, v := range resp.Buttons {
+		buttons = append(buttons, tgbotapi.NewKeyboardButton(v))
 	}
-	if resp.Keys != nil {
-		msg.ReplyMarkup = tgbotapi.NewReplyKeyboard(resp.Keys)
+
+	if resp.Buttons != nil {
+		msg.ReplyMarkup = tgbotapi.NewReplyKeyboard(buttons)
 	}
 	return msg
 }
