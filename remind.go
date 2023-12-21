38469718cca2d8772718da602e47b8e242227526
Yevgeni Starostin <erafaschismus@gmail.com>
2023-12-21 11:43:31 +0300

    refactor: use function

--- remind.go
+++ remind.go
@@ -10,14 +10,11 @@ func text(drugName string) string {
 
 func (j *Job) remind(app *App) {
 	db := app.DB
-	bot := app.Bot
 	setUserState(j.ChatID, UnderRemind, db)
 	resp := Response{
 		Text:    text(j.Name),
 		Buttons: []string{"Принял(а)"},
 		ChatID:  int64(j.ChatID),
 	}
-	msg := makeMessage(resp)
-	_, err := bot.Send(msg)
-	handleError(err)
+	app.send(resp)
 }
